fd8ae63b880607f0594ae6b4699dd92ed0840eac
DanielFordHUB <105242930+DanielFordHUB@users.noreply.github.com>
2022-08-25 12:11:55 -0500

    updated prepare

--- prepare.py
+++ prepare.py
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import numpy as np
-import pandas as pd
-import datetime
-
-
-
-def filter_students(df):
-    
-    # remove staff by cohort name
-    std = df.where(df['cohort_name'] != 'Staff').dropna()
-    
-    # create mask to filter amount of times accessed to 10 or below
-    mask = std['user_id'].value_counts()
-    std10less = std[std['user_id'].isin(mask.index[mask<11])]
-    
-    # create mask to filter by times accessed when students were active
-    std10less['date_mask'] = std10less.index >= pd.Timestamp('2019-11-04')
-    std_final = std10less.mask(std10less['date_mask'] == False).dropna()
-
-    # drop mask column
-    std_final = std_final.drop(columns=['date_mask'])
-    
-    # send it
-=======
 import numpy as np
 import pandas as pd
 import datetime
@@ -43,5 +18,24 @@ def filter_students(df):
     std_final = std10less.mask(std10less['date_mask'] == False).dropna()
     
     # send it
->>>>>>> 176d34eee9659361516fee8335cad499a8dfbefc
     return std_final
+
+
+
+def filter_grads(df):
+    
+    # Filter graduates by acesss after end date
+    df['mask'] = df.index >= df['end_date']
+    grads = df.mask(df['mask'] != True).dropna()
+    
+    # remove codeup staff, whether or not they graduated 
+    # codeups curriculum it biases the data as current employees
+    no_staff = grads.where(grads['cohort_name'] != 'Staff').dropna()
+    
+    # seperate by program
+    FSJ = no_staff.where(no_staff['program'] == 'Full Stack Java').dropna()
+    FSPHP = no_staff.where(no_staff['program'] == 'Full Stack PHP').dropna()
+    DS = no_staff.where(no_staff['program'] == 'Data Science').dropna()
+    F_E = no_staff.where(no_staff['program'] == 'Front-End').dropna()
+    
+    return FSJ, FSPHP, DS, F_E
